f9e7fd79fb26444339f7c575787d183b61addbb0
Norbi Bereczki <bnorbertcristian@gmail.com>
2020-09-09 10:19:21 +0300

    12

--- train.py
+++ train.py
@@ -39,8 +39,8 @@ cfg.DATASETS.TRAIN = ("fruits_nuts",)
 cfg.DATASETS.TEST = ()   # no metrics implemented for this dataset
 cfg.DATALOADER.NUM_WORKERS = 2
 # initialize from model zoo
-cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url(
-    "COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")
+# cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url(
+#    "COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")
 cfg.SOLVER.IMS_PER_BATCH = 2
 cfg.SOLVER.BASE_LR = 0.02
 cfg.SOLVER.MAX_ITER = 300    # 300 iterations seems good enough, but you can certainly train longer
