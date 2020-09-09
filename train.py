8707cd3bf4e82944ef593731fe119e2d5263a257
Norbi Bereczki <bnorbertcristian@gmail.com>
2020-09-09 08:42:49 +0300

    first commit

---
+++ train.py
@@ -0,0 +1,39 @@
+# https://colab.research.google.com/github/Tony607/detectron2_instance_segmentation_demo/blob/master/Detectron2_custom_coco_data_segmentation.ipynb#scrollTo=Lnkg1PByUjGQ
+
+from detectron2 import model_zoo
+from detectron2.config import get_cfg
+from detectron2.engine import DefaultTrainer
+from detectron2.data.datasets import register_coco_instances
+import os
+import zipfile
+
+INPUTS_DIR = os.getenv('VH_INPUTS_DIR', './inputs')
+data_path = os.path.join(INPUTS_DIR, 'data.zip')
+
+with zipfile.ZipFile(data_path, 'r') as zip_ref:
+    zip_ref.extractall('./data')
+
+
+register_coco_instances("fruits_nuts", {}, "./data/trainval.json", "./data/images")
+
+fruits_nuts_metadata = MetadataCatalog.get("fruits_nuts")
+dataset_dicts = DatasetCatalog.get("fruits_nuts")
+
+
+cfg = get_cfg()
+cfg.merge_from_file(model_zoo.get_config_file("COCO-Detection/mask_rcnn_R_50_FPN_3x.yaml"))
+cfg.DATASETS.TRAIN = ("fruits_nuts",)
+cfg.DATASETS.TEST = ()   # no metrics implemented for this dataset
+cfg.DATALOADER.NUM_WORKERS = 2
+# initialize from model zoo
+cfg.MODEL.WEIGHTS = "detectron2://COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x/137849600/model_final_f10217.pkl"
+cfg.SOLVER.IMS_PER_BATCH = 2
+cfg.SOLVER.BASE_LR = 0.02
+cfg.SOLVER.MAX_ITER = 300    # 300 iterations seems good enough, but you can certainly train longer
+cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 128   # faster, and good enough for this toy dataset
+cfg.MODEL.ROI_HEADS.NUM_CLASSES = 3  # 3 classes (data, fig, hazelnut)
+
+os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
+trainer = DefaultTrainer(cfg)
+trainer.resume_or_load(resume=False)
+trainer.train()
